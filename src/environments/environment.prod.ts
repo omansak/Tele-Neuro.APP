3dd9d5965e53c8ed216ed7c9a4a9a39287b1b1d6
Senshi #1 <omansakosk@gmail.com>
2021-12-30 16:45:40 +0300

    added prod configs

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,62 @@
 export const environment = {
-  production: true
+  production: true,
+  request: {
+    host: "https://api.telenororehab.com/",
+    rootEndPoint: "api/",
+    endPoints: {
+      category: {
+        updateCategory: "Category/UpdateCategory",
+        listCategories: "Category/ListCategories",
+        listActiveCategories: "Category/ListActiveCategories",
+        getCategory: "Category/GetCategory",
+        toggleCategoryStatus: "Category/ToggleCategoryStatus",
+      },
+      exercise: {
+        updateExercise: "Exercise/UpdateExercise",
+        listExercises: "Exercise/ListExercises",
+        toggleExerciseStatus: "Exercise/ToggleExerciseStatus",
+        searchExercises: "Exercise/SearchExercises",
+      },
+      program: {
+        updateProgram: "Program/UpdateProgram",
+        toggleProgramStatus: "Program/ToggleProgramStatus",
+        listPrograms: "Program/ListPrograms",
+        listProgramsByCategory: "Program/listProgramsByCategory",
+        assignExercise: "Program/AssignExercise",
+        assignedExercises: "Program/AssignedExercises",
+        changeSequenceAssignedExercise: "Program/ChangeSequenceAssignedExercise",
+        deleteAssignedExercise: "Program/DeleteAssignedExercise",
+        assignUser: "Program/AssignUser",
+        deleteAssignedUser: "Program/DeleteAssignedUser",
+        listAssignedUsers: "Program/ListAssignedUsers",
+        programInfo: "Program/ProgramInfo",
+      },
+      content: {
+        programInfo: "Content/ProgramInfo",
+        assignedExercises: "Content/AssignedExercises",
+        getActiveExercise: "Content/GetActiveExercise",
+        selfAssignedPrograms: "Content/SelfAssignedPrograms",
+      },
+      utility: {
+        listExercisePropertyDefinitions: "Utility/ListExercisePropertyDefinitions",
+        listRoleDefinitions: "Utility/ListRoleDefinitions"
+      },
+      stat: {
+        insertRelationStatLog: "Stat/InsertRelationStatLog",
+        completedExercisesOfProgram: "Stat/CompletedExercisesOfProgram",
+        userStats: "Stat/UserStats",
+      },
+      login: {
+        login: "Login/Login",
+        logout: "Login/Logout",
+        refreshToken: "Login/RefreshToken",
+      },
+      user: {
+        listFilterUsers: "User/ListFilterUsers",
+        addUser: "User/AddUser",
+        toggleUserStatus: "User/ToggleUserStatus",
+      }
+    }
+  }
 };
+
