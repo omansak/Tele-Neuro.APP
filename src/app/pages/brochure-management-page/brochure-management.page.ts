ceeedb0f462a7347d06a4adff53fb176cedbc840
Senshi #1 <omansakosk@gmail.com>
2022-01-16 23:21:59 +0300

    Update brochure-management.page.ts

--- src/app/pages/brochure-management-page/brochure-management.page.ts
+++ src/app/pages/brochure-management-page/brochure-management.page.ts
@@ -70,6 +70,7 @@ export class BrochureManagementPage implements AfterViewInit {
             let brochure = this.brochures.find(i => i.Brochure.Id == id);
             if (brochure) {
               brochure.Brochure.IsActive = !brochure.Brochure.IsActive;
+              this._changeDetectionRef.detectChanges();
               toast.success();
             }
             else {
